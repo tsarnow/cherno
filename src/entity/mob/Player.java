1294ac4b41e3662d1a0b5c6ba64392e2b548c502
Tobias Sarnow <tobias.sarnow@exxeta.com>
2014-01-22 22:20:26 +0100

    player animation fixed keyboard input error

--- src/entity/mob/Player.java
+++ src/entity/mob/Player.java
@@ -8,7 +8,9 @@ public class Player extends Mob {
 	
 	private Keyboard input;
 	private Sprite sprite;
-	
+	private int anim = 0;
+	private boolean walking = false;
+	private int step = 0;
 
 	public Player(Keyboard input) {
 		this.input = input;
@@ -24,20 +26,83 @@ public class Player extends Mob {
 	@Override
 	public void update() {
 		int xa=0, ya=0;
+		if (anim < 7500) anim++; 
+		else anim = 0;
 		if (input.up) ya--;
 		if (input.down) ya++;
 		if (input.left) xa--;
 		if (input.right) xa++;
 		
-		if (xa != 0 || ya != 0) move (xa, ya);
+		if (xa != 0 || ya != 0) { 
+			move (xa, ya);
+			walking = true;
+		} else {			
+			walking = false;
+			step = 0;
+		}
 	}
 
+	int flip = 0;
 	@Override
 	public void render(Screen screen) {
-		if (dir == 0) sprite = Sprite.player_back;
-		if (dir == 1) sprite = Sprite.player_right;
-		if (dir == 2) sprite = Sprite.player_forward;
-		if (dir == 3) sprite = Sprite.player_left;
-		screen.renderPlayer(x - 16, y - 16, sprite);
+		flip = 0;
+		if (dir == 0) {
+			sprite = moveAnimationPlayer(Sprite.player_back, Sprite.player_back_1, Sprite.player_back_2);
+		}
+		if (dir == 1) {
+			sprite = Sprite.player_side;
+			if (walking) {
+				if (anim % 20 > 10) {
+					sprite = Sprite.player_side_1;
+				} else {
+					sprite = Sprite.player_side_2;
+				}
+			}
+			flip = 1;
+		}
+		if (dir == 2) {
+			sprite = Sprite.player_forward;
+			if (walking) {
+				if (anim % 20 > 10) {
+					sprite = Sprite.player_forward_1;
+				} else {
+					sprite = Sprite.player_forward_2;
+				}
+			}
+		}
+		if (dir == 3) {
+			sprite = Sprite.player_side;
+			if (walking) {
+				if (anim % 20 > 10) {
+					sprite = Sprite.player_side_1;
+				} else {
+					sprite = Sprite.player_side_2;
+				}
+			}
+		}
+		screen.renderPlayer(x - 16, y - 16, sprite, flip);
+	}
+
+	private Sprite moveAnimationPlayer(Sprite stand,
+			Sprite step1, Sprite step2) {
+		Sprite sprite = stand;
+		if (walking) {
+			if (anim % 10 == 0) {
+				if (step > 3) step = 0;
+				step++;
+				//System.out.println(step);
+			}
+			if (anim % 20 > 10) {
+				sprite = step1;
+				flip = 1;
+//				if (step == 0) flip = 0; 
+				if (step == 3) flip = 1;
+			} else {
+				sprite = step2;
+				if (step == 1) flip = 0; 
+//				if (step == 2) flip = 1;
+			}
+		}
+		return sprite;
 	}
 }
--- src/graphics/Sprite.java
+++ src/graphics/Sprite.java
@@ -12,8 +12,17 @@ public class Sprite {
 	
 	public static Sprite player_forward = new Sprite(32, 0, 4, SpriteSheet.tiles);
 	public static Sprite player_back = new Sprite(32, 0, 6, SpriteSheet.tiles);
-	public static Sprite player_left = new Sprite(32, 0, 5, SpriteSheet.tiles);
-	public static Sprite player_right = new Sprite(32, 0, 7, SpriteSheet.tiles);
+	public static Sprite player_side = new Sprite(32, 0, 5, SpriteSheet.tiles);
+//	public static Sprite player_right = new Sprite(32, 0, 7, SpriteSheet.tiles);
+	
+	public static Sprite player_forward_1 = new Sprite(32, 1, 4, SpriteSheet.tiles);
+	public static Sprite player_forward_2 = new Sprite(32, 2, 4, SpriteSheet.tiles);
+
+	public static Sprite player_side_1 = new Sprite(32, 1, 5, SpriteSheet.tiles);
+	public static Sprite player_side_2 = new Sprite(32, 2, 5, SpriteSheet.tiles);
+
+	public static Sprite player_back_1 = new Sprite(32, 1, 6, SpriteSheet.tiles);
+	public static Sprite player_back_2 = new Sprite(32, 2, 6, SpriteSheet.tiles);
 	
 	public Sprite(int size, int x, int y, SpriteSheet sheet) {
 		this.SIZE = size;
